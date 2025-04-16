198d877560759aa24680fc05c6f14de2ece02192
Frankincense <frankincensewelsey@gmail.com>
2025-04-16 20:55:51 +0300

    unit testing

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,25 @@
 // Your tests here
+import {isPalindrome} from "../utils"
+
+describe("isPalindrome", ()=>{
+    test("takes in an word and checks if it is a palindrome",()=>{
+        const word = "racecar";
+        const result = isPalindrome(word)
+        expect(result).toBe(true)
+    })
+    test("takes in a mixed case word and checks if it is a palindrome",()=>{
+        const word = "RaCEcar";
+        const result = isPalindrome(word)
+        expect(result).toBe(true)
+    })
+    test("returns false when input is empty",()=>{
+        const word = " ";
+        const result = isPalindrome(word)
+        expect(result).toBe(false)
+    })
+    // test("throws errors when input had non-alphbetic characters",()=>{
+    //     const word = "2132";
+    //     const result = /^[A-Za-z]+$/.test(word)
+    //     expect(result).toThrow("Not a string")
+    // })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
 // Your code here
+export function isPalindrome(word){
+    if(!word || word === " "){
+        return false
+    }
+    let end = word.length -1 
+    for (let i = 0; i < end; i++){
+        word[i] !== word[end] ? false : end--
+    }
+    return true
+}
